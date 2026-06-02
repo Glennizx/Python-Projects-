3e7a67bcb4c8f7101c9da0eceb9b671f571cb580
Gelade <glennezekielmiral@gmail.com>
2026-06-02 06:02:54 +0000

    commit

---
+++ 23.. Quiz game.py
@@ -0,0 +1,18 @@
+#Python quiz game
+
+
+questions = ("How many elements are in the periodic table?: ",
+             "Which animal lays the largest eggs?: ",
+             "What is the most abundant gas in Earth's atmosphere?: ",
+             "How many bones are in the human body?: ",
+             "Which planet in the solar system is the hottest?: "
+
+)
+
+options = ((),(),(),(),())
+
+
+answers = ()
+quesses = []
+score = 0
+question_num = 0
