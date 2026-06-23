44bd69e3e99a0bddebab95c407055585a82c2d85
Glenn Ezekiel Miral <glennezekielmiral@gmail.com>
2026-06-23 23:41:10 +0000

    commit

--- Learning Materials/BroCode - Youtube/23.. Quiz game.py
+++ Learning Materials/BroCode - Youtube/23.. Quiz game.py
@@ -9,10 +9,18 @@ questions = ("How many elements are in the periodic table?: ",
 
 )
 
-options = ((),(),(),(),())
+options = (("A. 116","B. 117","C. 118", "D. 119"),
+           ("A. Whale","B. Crocodile","C. Elephant", "D. Ostrich"),
+           ("A. Nitrogen","B. Oxygen","C. Carbon-Dioxide", "D. Hydrogen"),
+           ("A. 206","B. 207","C. 208", "D. 209"),
+           ("A. Mercury","B. Venus","C. Earth", "D. Mars"))
 
 
-answers = ()
+answers = ("C", "D", "A", "A", "B")
 quesses = []
 score = 0
 question_num = 0
+
+for question in questions:
+    print("---------------")
+    print(question)
