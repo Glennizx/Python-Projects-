1cf663536d5934437d414d5afbba70c244b3c4fe
Glenn Ezekiel Miral <glennezekielmiral@gmail.com>
2026-06-24 01:31:34 +0000

    commit

--- Learning Materials/BroCode - Youtube/23.. Quiz game.py
+++ Learning Materials/BroCode - Youtube/23.. Quiz game.py
@@ -13,14 +13,45 @@ options = (("A. 116","B. 117","C. 118", "D. 119"),
            ("A. Whale","B. Crocodile","C. Elephant", "D. Ostrich"),
            ("A. Nitrogen","B. Oxygen","C. Carbon-Dioxide", "D. Hydrogen"),
            ("A. 206","B. 207","C. 208", "D. 209"),
-           ("A. Mercury","B. Venus","C. Earth", "D. Mars"))
+           ("A. Mercury","B. Venus","C. Earth", "D. Mars"))  #2D tuple
 
 
 answers = ("C", "D", "A", "A", "B")
-quesses = []
+guesses = []
 score = 0
 question_num = 0
 
 for question in questions:
     print("---------------")
     print(question)
+    for option in options[question_num]:
+        print(option)
+        question_num += 0
+    
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT!")
+
+    
+    else:
+        print("INCORRECT!")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num += 1
+
+print("---------------")
+print("      RESULTS        ")
+print("---------------")
+
+
+print("answers: ", end="")
+for guess in guesses:
+    print(guess, end=" ")
+
+print()
+
+score = int(score / len(questions) * 100)
+print(f"Your score is: {score}%")
+
+
